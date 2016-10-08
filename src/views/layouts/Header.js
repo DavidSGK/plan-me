0b81159af10830e7b1e014bb9e3d042a1e47fd2c
DavidSGK <Bassexe97>
2016-10-08 13:22:59 -0400

    Landing Page v1

--- src/views/layouts/Header.js
+++ src/views/layouts/Header.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import { connect } from 'react-redux';
 
+const headerStyle = {
+  position: 'fixed',
+};
+
 const Header = ({isLoggedIn}) => (
   <AppBar
+    style={headerStyle}
     title="Can U Plan Me"
     showMenuIconButton={isLoggedIn}
   />
