0ee0368136fc7836f49eba9606c84323362b1f36
Wilson Wu <wilsoncwu@gmail.com>
2016-10-08 04:55:24 -0400

    login module is added to the top half of the code. it auto-opens a popup that checks the user, and asks to sign in if not already. user-id is then printed through console, but can now be used to store user data. WILL NOT RUN UNLESS ACTUALL HTTP

--- event.js
+++ event.js
@@ -9,8 +9,73 @@ var config = {
 };
 
 firebase.initializeApp(config);
-
 var db = firebase.database();
+
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) {
+    // [START createprovider]
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // [END createprovider]
+    // [START addscopes]
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    // [END addscopes]
+    // [START signin]
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // [START_EXCLUDE]
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('You have already signed up with a different auth provider for that email.');
+        // If you are using multiple auth providers on your app you should handle linking
+        // the user's accounts here.
+      } else {
+        console.error(error);
+      }
+      // [END_EXCLUDE]
+    });
+    // [END signin]
+  } else {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  }
+}
+function initApp(){
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+		  // User is signed in.
+		  var displayName = user.displayName;
+		  var email = user.email;
+		  var emailVerified = user.emailVerified;
+		  var photoURL = user.photoURL;
+		  var isAnonymous = user.isAnonymous;
+		  var uid = user.uid;
+		  var providerData = user.providerData;
+		  console.log(uid);
+		}
+	});	
+}
+
+// [END buttoncallback]
+
+window.onload = function() {
+	toggleSignIn();
+	initApp();
+}
+
+
+
+
 
 createEventArray("wilson");
 
@@ -33,8 +98,5 @@ function createEventArray(user){
 		evArray.forEach(function(event){
 			console.log(event);
 		});
-		return evArray;
 	});
-		
-
 }
