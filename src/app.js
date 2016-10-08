4fdef81a8c63e6033feaf0ab87600e118c3f665a
Lawrence Pan <lawrencefeipan@gmail.com>
2016-10-08 09:01:40 -0400

    add material ui

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import store from './store';
 import Routes from './routes';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 ReactDOM.render(
-  <Provider store={store} >
-    <Routes />
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store} >
+      <Routes />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
+const styles = {
+  textField: {
+  },
+  textBox: {
+    textAlign: 'center',
+  },
+};
+
+const USERNAME = 'Username';
 
 const Home = () => (
   <div>
-    <p>Home</p>
+    <Paper style={styles.textBox} zDepth={2}>
+      <TextField
+        hintText={USERNAME}
+        floatingLabelText={USERNAME}
+      />
+      <TextField
+        hintText={USERNAME}
+        floatingLabelText={USERNAME}
+      />
+    </Paper>
   </div>
 );
 
