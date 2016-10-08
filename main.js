502ac305fd8c7b3ff15f5adfff0d59830e7c5b5e
Alex Foley <foleyfactor98@gmail.com>
2016-10-08 08:14:14 -0400

    Added function to rearrange schedule
    Humans can tweak their schedule now

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+Object.compare = function (obj1, obj2) {
+	if (!obj1 && !obj2) return true;
+	if (!obj1 && obj2) return false;
+	if (obj1 && !obj2) return false;
+
+	for (var p in obj1) {
+		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+ 
+		switch (typeof (obj1[p])) {
+			case 'object':
+				if (!Object.compare(obj1[p], obj2[p])) return false;
+				break;
+			case 'function':
+				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+				break;
+			default:
+				if (obj1[p] != obj2[p]) return false;
+		}
+	}
+	for (var p in obj2) {
+		if (typeof (obj1[p]) == 'undefined') return false;
+	}
+	return true;
+};
 
 // Initialize Firebase
 var config = {
@@ -8,7 +32,7 @@ var config = {
 	messagingSenderId: "751157862033"
 };
 
-var NUM_INTERVAL = 10;//672
+var NUM_INTERVAL = 14;//672
 
 firebase.initializeApp(config);
 
@@ -52,7 +76,7 @@ function layoutThings(lst) {
 		var a = largestSubarrayIndex(cal);
 		var max = a[0], ind = a[1];
 		if (lst[i]['duration'] <= max) {
-			place(cal, lst[i]['duration'], ind, lst[i]['id']);
+			place(cal, lst[i]['duration'], ind, lst[i]);
 			cal = cal;
 		} else if (max == 0) {
 			break;
@@ -136,11 +160,56 @@ function merge(l1, l2) {
 	return lt;
 }
 
+//Can take event or id, need to decide.
+function moveDown(calendar, event) {
+	var aFound=false, bFound = false;
+	var aStart=0, bStart=0;
+	var bEnd=0, b=-1;
+
+	for (var i=0; i<calendar.length; i++) {
+		if (! aFound && Object.compare(calendar[i], event)) {
+			aFound = true;
+			aStart = i;
+		} else if (aFound && ! bFound && ! Object.compare(calendar[i], event)) {
+			bFound = true;
+			bStart = i;
+			b = calendar[i];
+		} else if (aFound && bFound && (! Object.compare(calendar[i], b) || i == calendar.length-1)) {
+			bEnd = i;
+		}
+
+		if (aFound && i==calendar.length-1 && calendar[i] == null) {
+			bEnd = i+1;
+		}
+
+		if (! b) {
+			bEnd = i+1;
+			break;
+		}
+	}
+
+	if (! bFound) {
+		console.log('lmao');
+		return calendar;
+	}
+
+	for (var i=bEnd-aStart-1; i>=0; i--) {
+		if (bEnd-i-1 < bStart) {
+			calendar[aStart+i] = event;
+		} else {
+			calendar[aStart+i] = b;
+		}
+	}
+
+	return calendar;
+
+}
+
 var a = {'duration': 2, 'priority': 1, 'id': 1};
 var b = {'duration': 2, 'priority': 1, 'id': 2};
 var c = {'duration': 2, 'priority': 1, 'id': 3};
 var d = {'duration': 2, 'priority': 5, 'id': 4};
 var e = {'duration': 3, 'priority': 2, 'id': 5};
 
-
-console.log(layoutThings([a,b,c,d,e]));
+// console.log(Object.compare(a,b));
+// console.log(moveDown(moveDown(moveDown(moveDown(layoutThings([a,b,c,d,e]), d), d), d), d));
